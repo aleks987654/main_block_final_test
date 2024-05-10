14eec2d8ca453ab9f053ef16d8e8ef30a4232f24
Aleksandr Trushkov <aleksmount@yandex.ru>
2024-05-10 21:19:06 +0700

    task completed with example 1

--- Program.cs
+++ Program.cs
@@ -9,3 +9,51 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+using System;
+using System.Text;
+
+// Функция получения массива элементов, 
+// состоящих из трёх либо менее символов
+string[] Array3SymbolsElements(string[] array)
+{
+    int n = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) n++;
+    }
+
+    string[] sym3els = new string[n];
+    n = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) 
+        {
+            sym3els[n] = array[i];
+            n++;
+        }
+    }
+    
+    return sym3els;
+}
+
+// Функция вывода элементов массива на консоль
+void PrintArray(string[] array)
+{
+    foreach (var e in array)
+    {
+        Console.Write($"'{e}' ");
+    }
+    Console.WriteLine();
+}
+
+
+// string[] str_array = ["Hello", "2", "world", ":-)"];
+Console.WriteLine("Исходный массив:");
+PrintArray(["Hello", "2", "world", ":-)"]);
+
+string[] res = Array3SymbolsElements(["Hello", "2", "world", ":-)"]);
+Console.WriteLine("Массив элементов, состоящих из 3 либо менее символов:");
+PrintArray(res);
+
